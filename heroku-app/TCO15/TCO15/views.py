0686d91752884714e86896f2805977cc83fb80bd
Bai Li <luckytoilet@gmail.com>
2015-06-20 13:14:30 -0700

    integrate

--- heroku-app/TCO15/TCO15/views.py
+++ heroku-app/TCO15/TCO15/views.py
@@ -1,6 +1,3 @@
 from django.http import HttpResponse
-from win import *
-
 def homepage(request):
-  send_sms_to('+14156322541')
-  return HttpResponse("Message sent!")
+  return HttpResponse("Hi Niranjan!!!")
