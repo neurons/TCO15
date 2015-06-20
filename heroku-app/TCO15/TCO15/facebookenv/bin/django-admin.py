d057209d41307d72f3189904dec74525f90dd0b8
Nir Padmanabhan <niranjan.padmanabhan@gmail.com>
2015-06-20 15:26:55 -0400

    CSS + HTML added

---
+++ heroku-app/TCO15/TCO15/facebookenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bai-personal/Documents/TCO15/heroku-app/TCO15/TCO15/facebookenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
