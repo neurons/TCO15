d1ee254fd98f3cb3203c3ee9da9c64cd9d0dfe98
Bai Li <luckytoilet@gmail.com>
2015-06-19 22:58:24 -0700

    pretty printing of facebook event

--- win.py
+++ win.py
@@ -1,11 +1,14 @@
 import facebook
 import requests
 import yaml
+from datetime import datetime, timedelta
 
 # This should not be committed to a public repo. But this
 # is a hackathon so who gives a shit.
 APP_ID = '1667264776818224'
 APP_SECRET = '1be1d6fe492e70c79d2e397c16685152'
+STRPTIME_FORMAT = '%Y-%m-%dT%H:%M:%S-0700'
+SMS_FORMAT = '%s will run from %s to %s. Attending: %d'
 
 def get_fb_token(app_id, app_secret):           
   payload = {'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_secret}
@@ -23,12 +26,16 @@ def get_event_count(event_id):
 access_token = get_fb_token(APP_ID, APP_SECRET)
 graph = facebook.GraphAPI(access_token=access_token)
 
-# Try to access https://www.facebook.com/events/789305557821642/
-event_id = '789305557821642'
+# Try to access https://www.facebook.com/events/1041695122513908/
+event_id = '1041695122513908'
 event = graph.get_object(event_id)
 event_count = get_event_count(event_id)
-print "Name:", event['name']
-print "Start time:", event['start_time']
-print "End time:", event['end_time']
-print "Attending:", event_count
+event_name = event['name']
+event_start = datetime.strptime(event['start_time'], STRPTIME_FORMAT) + timedelta(hours=7)
+event_end = datetime.strptime(event['end_time'], STRPTIME_FORMAT) + timedelta(hours=7)
 
+# Format it nicely
+event_start_str = event_start.strftime("%B %d at %H:%M %p")
+event_end_str = event_end.strftime("%H:%M %p")
+sms_msg = SMS_FORMAT % (event_name, event_start_str, event_end_str, event_count)
+print sms_msg
